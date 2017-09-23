a27bbf4db40ab4a4e0ebdf83707cb7cbff842002
tugsbayasgalan <tugsuu@mit.edu>
2017-09-23 14:05:15 -0400

    Added initial specs

--- P1/q1.py
+++ P1/q1.py
@@ -1,4 +1,31 @@
 # First question for P1
 
+import numpy as np
 
-# TODO Implement gradient descent here 
+# TODO Implement gradient descent here
+
+def batch_gradient_descent(init, step_size, convergence, f, f_der):
+    """
+    init - initial guess
+    step_size - step size for the GD
+    f - objective function
+    f_der - derivative of the objective function
+
+    return x_min
+
+    """
+
+    pass
+
+def stochastic_gradient_descent(init, step_size, convergence, f, f_der):
+    """
+    init - initial guess
+    step_size - step size for the GD
+    f - objective function
+    f_der - derivative of the objective function
+
+    return x_min
+
+    """
+
+    pass
