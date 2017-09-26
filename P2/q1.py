1426f939b2a8a377dc47d2ea492d16a89f9fd912
janak2 <janakagrawal1309@gmail.com>
2017-09-26 19:56:34 -0400

    improved plots in q1

--- P2/q1.py
+++ P2/q1.py
@@ -3,16 +3,21 @@ from scipy.stats import multivariate_normal
 from loadFittingDataP2 import getData as parameters
 import matplotlib.pyplot as plt
 import pylab as pl
+from scipy.interpolate import spline
 
 
-def max_like_vector(X,Y,M):
+def compute_X(X,M):
 	r = np.zeros((X.size,M+1))
 	for i in range(X.size):
 		t = 1
 		for j in range(M+1):
 			r[i,j] = t
 			t *= X[i]
-	return (r,np.dot(np.linalg.inv(np.dot(np.transpose(r),r)),np.dot(np.transpose(r),Y)))
+	return r	
+
+def max_like_vector(X,Y,M):
+	r = compute_X(X,M)
+	return np.dot(np.linalg.inv(np.dot(np.transpose(r),r)),np.dot(np.transpose(r),Y))
 
 par = parameters(False)
 X = par[0]
@@ -21,13 +26,15 @@ Y = par[1]
 # Testing
 # TODO maybe trying different initial values ??
 if __name__ == '__main__':
-    #print gaussMean
-    r,m = max_like_vector(X,Y,3)
-    y = np.dot(r,m)
+	#print gaussMean
+	m = 10
 
-    plt.plot(X,Y,'o',X,y,'k')
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.show()
+	r = max_like_vector(X,Y,m)
+	xnew = np.linspace(X.min(),X.max(),300) #300 represents number of points to make between T.min and T.max
+	y = np.dot(compute_X(xnew,m),r)
+	plt.plot(X,Y,'o',xnew,y,'k')
+	plt.xlabel('x')
+	plt.ylabel('y')
+	plt.show()
 
 
